5f1c4b1e1caf5849684998983dd6161fc0413406
DigitalHerencia <DigitalHerencia@outlook.com>
2025-04-03 04:39:50 -0600

    style complete

--- components/ui/calendar.tsx
+++ components/ui/calendar.tsx
@@ -4,7 +4,7 @@ import * as React from "react"
 import { ChevronLeft, ChevronRight } from "lucide-react"
 import { DayPicker } from "react-day-picker"
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils/utils"
 import { buttonVariants } from "@/components/ui/button"
 
 function Calendar({
@@ -15,7 +15,6 @@ function Calendar({
 }: React.ComponentProps<typeof DayPicker>) {
   return (
     <DayPicker
-      showOutsideDays={showOutsideDays}
       className={cn("p-3", className)}
       classNames={{
         months: "flex flex-col sm:flex-row gap-2",
@@ -67,6 +66,7 @@ function Calendar({
           <ChevronRight className={cn("size-4", className)} {...props} />
         ),
       }}
+      showOutsideDays={showOutsideDays}
       {...props}
     />
   )
